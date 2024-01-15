8ac4b94f436cb13d5cd9641eec26438b620d9dd7
stehanget <fahrizasirojudin@gmail.com>
2024-01-15 20:28:58 +0700

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,30 +33,97 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Center(
-            child: Text(
-          widget.title,
-          style:
-              const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
-        )),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        margin: const EdgeInsets.fromLTRB(0, 24.0, 60.0, 0),
-        decoration: const BoxDecoration(color: Colors.blue),
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 320.0),
-          decoration: const BoxDecoration(color: Colors.green),
-          child: Container(
-            margin: const EdgeInsets.fromLTRB(0, 24.0, 160.0, 24.0),
-            padding: const EdgeInsets.all(40.0),
-            decoration: const BoxDecoration(color: Colors.yellow),
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+              child: Text(
+            widget.title,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w500),
+          )),
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                const Text(
+                  'SIAKAD UNSADA',
+                  style: TextStyle(color: Colors.blue, fontSize: 24.0),
+                ),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'User Name', border: OutlineInputBorder()),
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Password', border: OutlineInputBorder()),
+                ),
+                TextButton(
+                  onPressed: () => {},
+                  child: const Text(
+                    'Forgot Password',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                        padding: MaterialStatePropertyAll<
+                                EdgeInsetsDirectional>(
+                            EdgeInsetsDirectional.symmetric(vertical: 20.0)),
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.blue),
+                        shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2.0))))),
+                    onPressed: () => {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Does not have account?',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
